17f60649470398a52631e6ad35b36d6f096832f2
Gustavo <gustavo_carvalho150@estudante.sesisenai.org.br>
2026-07-17 20:26:47 -0300

    Exercicio 1 ok

---
+++ src/lista/Ex1.java
@@ -0,0 +1,16 @@
+package lista;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+    static void exibirSaudacao(String nome){
+        System.out.println("Olá " + nome + ", seja bem vindo");
+    }
+
+    void main(){
+        exibirSaudacao("Gustavo");
+        exibirSaudacao("Yarlei");
+        exibirSaudacao("Kossi");
+    }
+}
